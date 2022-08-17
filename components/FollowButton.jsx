235d3f5b1d3c0674c0fdb7137eee8d354dbc2839
Alexandre Carvalheira <axxcar@gmail.com>
2022-08-17 13:24:03 -0300

    Update FollowButton.jsx

--- components/FollowButton.jsx
+++ components/FollowButton.jsx
@@ -67,5 +67,12 @@ export default function FollowButton({ profileId }) {
     console.log("tx hash", tx.hash);
     return tx.hash;
   };
-  return <button onClick={follow}>Follow</button>;
+  return (
+    <button
+      className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"
+      onClick={follow}
+    >
+      Follow
+    </button>
+  );
 }
